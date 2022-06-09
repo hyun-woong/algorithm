a68629f0e6e36e245328148bd7088e05e56f36ff
hyun-woong <mr.woong0926@gmail.com>
2022-06-09 20:45:22 +0900

    백준 알고리즘

--- src/main/java/com/example/algorithm/level01/AB.java
+++ src/main/java/com/example/algorithm/level01/AB.java
@@ -3,21 +3,52 @@ package com.example.algorithm.level01;
 import java.util.Scanner;
 
 public class AB {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int A = scanner.nextInt();
+//        int B = scanner.nextInt();
+//        if (A > 0 && B < 10) {
+//            System.out.println(A+B);
+//        }
+//    }
+
+//    public static void main2(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int A = scanner.nextInt();
+//        int B = scanner.nextInt();
+//        if (A > 0 && B < 10) {
+//            System.out.println(A-B);
+//        }
+//    }
+//
+//    public static void main1(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int A = scanner.nextInt();
+//        int B = scanner.nextInt();
+//        if (A > 0 && B < 10) {
+//            System.out.println(A*B);
+//        }
+//    }
+
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        double A = scanner.nextInt();
+//        double B = scanner.nextInt();
+//        if (A > 0 && B < 10) {
+//            System.out.println(A/B);
+//        }
+//    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int A = scanner.nextInt();
         int B = scanner.nextInt();
-        if (A > 0 && B < 10) {
+        if (1 <= A && B <= 10000) {
             System.out.println(A+B);
-        }
-    }
-
-    public static void main2(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int A = scanner.nextInt();
-        int B = scanner.nextInt();
-        if (A > 0 && B < 10) {
             System.out.println(A-B);
+            System.out.println(A*B);
+            System.out.println(A/B);
+            System.out.println(A%B);
         }
     }
 }
