4314125451d8e24817db31666087aa6cabc81c95
hyun-woong <mr.woong0926@gmail.com>
2022-06-15 22:04:46 +0900

    백준 알고리즘 2단계 2번 완료

--- src/main/java/com/example/algorithm/level02/Main.java
+++ src/main/java/com/example/algorithm/level02/Main.java
@@ -3,18 +3,40 @@ package com.example.algorithm.level02;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int a = scanner.nextInt();
-        int b = scanner.nextInt();
 
-        if (a > b) {
-            System.out.println(">");
-        } else if (a < b){
-            System.out.println("<");
+        if (a >= 90) {
+            System.out.println("A");
+        } else if (a >= 80 && a <= 89){
+            System.out.println("B");
+        } else if (a >= 70 && a <= 79){
+            System.out.println("C");
+        } else if (a >= 60 && a <= 69) {
+            System.out.println("D");
         } else {
-            System.out.println("==");
+            System.out.println("F");
         }
+
     }
+
+
+
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//
+//        if (a > b) {
+//            System.out.println(">");
+//        } else if (a < b){
+//            System.out.println("<");
+//        } else {
+//            System.out.println("==");
+//        }
+//    }
 }
