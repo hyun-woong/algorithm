2e8ee3b52488f43fe08df07e09a0fd2f6c086bc2
hyun-woong <mr.woong0926@gmail.com>
2022-06-16 04:23:42 +0900

    백준 알고리즘 3단계 8번 완료

--- src/main/java/com/example/algorithm/level03/Main.java
+++ src/main/java/com/example/algorithm/level03/Main.java
@@ -14,9 +14,25 @@ public class Main {
 
             int answer = a + b;
 
-            System.out.println("Case #" + i + ":  " + answer);
+            System.out.println("Case #" + i + ":  " + a + " + " + b + " = " + answer);
         }
     }
+
+
+
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int n = scanner.nextInt();
+//
+//        for (int i = 1; i <= n; i++) {
+//            int a = scanner.nextInt();
+//            int b = scanner.nextInt();
+//
+//            int answer = a + b;
+//
+//            System.out.println("Case #" + i + ":  " + answer);
+//        }
+//    }
 
 
 
