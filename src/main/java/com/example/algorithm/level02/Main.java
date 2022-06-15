db0d801e98ff1f1b82ec2225db7ac5895a145aed
hyun-woong <mr.woong0926@gmail.com>
2022-06-15 22:16:49 +0900

    백준 알고리즘 2단계 4번 완료

--- src/main/java/com/example/algorithm/level02/Main.java
+++ src/main/java/com/example/algorithm/level02/Main.java
@@ -7,18 +7,34 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int a = scanner.nextInt();
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
 
-        if (a%4 == 0 && a%100 != 0 || a%400 ==0){
+        if (x > 0 && y > 0){
             System.out.println("1");
-        }else {
-            System.out.println("0");
+        } else if (x < 0 && y > 0) {
+            System.out.println("2");
+        } else if (x < 0 && y < 0) {
+            System.out.println("3");
+        } else {
+            System.out.println("4");
         }
     }
 
 
 
 
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int a = scanner.nextInt();
+//
+//        if (a%4 == 0 && a%100 != 0 || a%400 ==0){
+//            System.out.println("1");
+//        }else {
+//            System.out.println("0");
+//        }
+//    }
 
 //    public static void main(String[] args) {
 //        Scanner scanner = new Scanner(System.in);
