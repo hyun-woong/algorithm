bbb6ae24ab95c4e2dc83e39ebf99b4bd879ee18e
hyun-woong <mr.woong0926@gmail.com>
2022-06-16 15:17:21 +0900

    백준 알고리즘 3단계 9번 완료

--- src/main/java/com/example/algorithm/level03/Main.java
+++ src/main/java/com/example/algorithm/level03/Main.java
@@ -4,19 +4,34 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int n = scanner.nextInt();
 
-        for (int i = 1; i <= n; i++) {
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-
-            int answer = a + b;
-
-            System.out.println("Case #" + i + ":  " + a + " + " + b + " = " + answer);
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.printf("*");
+                }
+                System.out.println();
+            }
         }
-    }
+
+
+
+
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int n = scanner.nextInt();
+//
+//        for (int i = 1; i <= n; i++) {
+//            int a = scanner.nextInt();
+//            int b = scanner.nextInt();
+//
+//            int answer = a + b;
+//
+//            System.out.println("Case #" + i + ":  " + a + " + " + b + " = " + answer);
+//        }
+//    }
 
 
 
