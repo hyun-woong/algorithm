928f28874c710f1881efb2ba2d297e19c29c3e3b
hyun-woong <mr.woong0926@gmail.com>
2022-06-16 16:56:21 +0900

    백준 알고리즘 3단계 11번 완료

--- src/main/java/com/example/algorithm/level03/Main.java
+++ src/main/java/com/example/algorithm/level03/Main.java
@@ -4,20 +4,38 @@ import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        int x = scanner.nextInt();
 
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n - i; j++) {
-                    System.out.print(" ");
-                }
-                for (int z = 0; z < i; z++) {
-                    System.out.print("*");
-                }
-                System.out.println();
+        for (int i = 0; i < n; i++) {
+            int a = scanner.nextInt();
+            if (a < x) {
+                System.out.println(a);
             }
         }
+    }
+
+
+
+
+
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int n = scanner.nextInt();
+//
+//            for (int i = 1; i <= n; i++) {
+//                for (int j = 1; j <= n - i; j++) {
+//                    System.out.print(" ");
+//                }
+//                for (int z = 0; z < i; z++) {
+//                    System.out.print("*");
+//                }
+//                System.out.println();
+//            }
+//        }
 
 
 
