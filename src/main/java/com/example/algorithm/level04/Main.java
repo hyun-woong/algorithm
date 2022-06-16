eb52980a81ad56aa97bed43f3a8c5db45a85c408
hyun-woong <mr.woong0926@gmail.com>
2022-06-17 00:23:26 +0900

    백준 알고리즘 4단계 2 완료

--- src/main/java/com/example/algorithm/level04/Main.java
+++ src/main/java/com/example/algorithm/level04/Main.java
@@ -5,12 +5,12 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
         List<Integer> integerList = new ArrayList<>();
 
-        for (int i = 0; i < x; i++) {
+        for (int i = 0; i < 9; i++) {
             int a = scanner.nextInt();
             integerList.add(a);
         }
@@ -20,11 +20,30 @@ public class Main {
                 .max()
                 .getAsInt();
 
-        Integer min = integerList.stream()
-                .mapToInt(y -> y)
-                .min()
-                .getAsInt();
-        System.out.println(min);
         System.out.println(max);
+        System.out.println(integerList.indexOf(max) + 1);
     }
+
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int x = scanner.nextInt();
+//        List<Integer> integerList = new ArrayList<>();
+//
+//        for (int i = 0; i < x; i++) {
+//            int a = scanner.nextInt();
+//            integerList.add(a);
+//        }
+//
+//        Integer max = integerList.stream()
+//                .mapToInt(y -> y)
+//                .max()
+//                .getAsInt();
+//
+//        Integer min = integerList.stream()
+//                .mapToInt(y -> y)
+//                .min()
+//                .getAsInt();
+//        System.out.println(min);
+//        System.out.println(max);
+//    }
 }
