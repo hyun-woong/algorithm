254f95a981c3124bd32a355da812526af807e716
hyun-woong <mr.woong0926@gmail.com>
2022-06-16 17:59:22 +0900

    백준 알고리즘 3단계 올클

--- src/main/java/com/example/algorithm/level03/Main.java
+++ src/main/java/com/example/algorithm/level03/Main.java
@@ -7,16 +7,54 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
+        int x = scanner.nextInt();
+        int cnt = 0;
+        int N = x;
 
-            if (a == 0 && b == 0) {
+        while (true) {
+            N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+            cnt++;
+            if (x == N) {
+                System.out.println(cnt);
                 break;
             }
-            System.out.println(a + b);
-        }
+            }
     }
+
+
+
+
+
+
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        while (scanner.hasNextInt()) {
+//            int a = scanner.nextInt();
+//            int b = scanner.nextInt();
+//
+//            System.out.println(a + b);
+//        }
+//        scanner.close();
+//
+//    }
+
+
+
+
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        while (true) {
+//            int a = scanner.nextInt();
+//            int b = scanner.nextInt();
+//
+//            if (a == 0 && b == 0) {
+//                break;
+//            }
+//            System.out.println(a + b);
+//        }
+//    }
 
 
 
