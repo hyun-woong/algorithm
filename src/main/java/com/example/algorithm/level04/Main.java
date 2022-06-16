c875127c8b8ad57622fc81ac88d9556b3ef4d6f1
hyun-woong <mr.woong0926@gmail.com>
2022-06-17 01:27:49 +0900

    백준 알고리즘 4단계 3 완료

--- src/main/java/com/example/algorithm/level04/Main.java
+++ src/main/java/com/example/algorithm/level04/Main.java
@@ -1,28 +1,64 @@
 package com.example.algorithm.level04;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<Integer> integerList = new ArrayList<>();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
 
-        for (int i = 0; i < 9; i++) {
-            int a = scanner.nextInt();
-            integerList.add(a);
+        int x = a * b * c;
+
+        String y = String.valueOf(x);
+        List<String> arrList = new ArrayList<>();
+
+        Map<String, Integer> map = new HashMap<String, Integer>();
+
+        for (int i = 0; i < y.length(); i++) {
+            String z = String.valueOf(y.charAt(i));
+            arrList.add(z);
         }
 
-        Integer max = integerList.stream()
-                .mapToInt(y -> y)
-                .max()
-                .getAsInt();
+        for (String arr : arrList) {
+            Integer cnt = map.get(arr);
+            if (cnt == null) {
+                map.put(arr, 1);
+            } else {
+                map.put(arr, cnt +1);
+            }
+        }
 
-        System.out.println(max);
-        System.out.println(integerList.indexOf(max) + 1);
+        for (int i = 0; i < 10; i++) {
+            String v = String.valueOf(i);
+            if (map.get(v) == null){
+                System.out.println("0");
+            } else {
+                System.out.println(map.get(v));
+            }
+        }
     }
+
+
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        List<Integer> integerList = new ArrayList<>();
+//
+//        for (int i = 0; i < 9; i++) {
+//            int a = scanner.nextInt();
+//            integerList.add(a);
+//        }
+//
+//        Integer max = integerList.stream()
+//                .mapToInt(y -> y)
+//                .max()
+//                .getAsInt();
+//
+//        System.out.println(max);
+//        System.out.println(integerList.indexOf(max) + 1);
+//    }
 
 //    public static void main(String[] args) {
 //        Scanner scanner = new Scanner(System.in);
