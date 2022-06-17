bf5962bd5784c537888085e5482c91e2521eeb3d
hyun-woong <mr.woong0926@gmail.com>
2022-06-17 15:30:56 +0900

    백준 알고리즘 문자열 4번 완료

--- src/main/java/com/example/algorithm/level05/Main.java
+++ src/main/java/com/example/algorithm/level05/Main.java
@@ -1,15 +1,52 @@
 package com.example.algorithm.level05;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char N = br.readLine().charAt(0);
-        byte num = (byte) N;
-        System.out.println(num);
+
     }
+
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        int n = Integer.parseInt(br.readLine());
+//        for (int i = 0; i < n; i++) {
+//            String x = br.readLine(); // 문자열
+//            int a = Integer.parseInt(x.split(" ")[0]);
+//            StringBuilder answer = new StringBuilder();
+//            char[] arr = x.split(" ")[1].toCharArray();
+//            for (char c : arr) {
+//                for (int z = 0; z < a; z++) {
+//                    answer.append(c);
+//                }
+//            }
+//            System.out.println(answer.toString());
+//        }
+//    }
+
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int n = Integer.parseInt(br.readLine());
+//        String t = br.readLine();
+//
+//        char[] arr = t.toCharArray();
+//        int cnt = 0;
+//
+//        for (char w : arr) {
+//            int cw = Integer.parseInt(String.valueOf(w));
+//            cnt += cw;
+//        }
+//        System.out.println(cnt);
+//    }
+
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        char N = br.readLine().charAt(0);
+//        byte num = (byte) N;
+//        System.out.println(num);
+//    }
 }
 
